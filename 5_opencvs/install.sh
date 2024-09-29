558c74ce5e8dfa29904061c5a67ac449e367c955
adityakhode <aditya_khode_entc@moderncoe.edu.in>
2024-09-30 00:16:40 +0530

    added opencv installation

---
+++ 5_opencvs/install.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install libjpeg-dev libpng-dev libtiff-dev
+sudo apt install libatlas-base-dev gfortran
+sudo apt install libopencv-dev
+pkg-config --modversion opencv4
